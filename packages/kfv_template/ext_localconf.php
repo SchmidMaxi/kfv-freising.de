35b6da6b21cd11544a3f519ba4927a779bebc5d6
Maximilian Schmid <maximilian.schmid@trionline.de>
2025-01-29 16:53:00 +0100

    Next step

--- packages/kfv_template/ext_localconf.php
+++ packages/kfv_template/ext_localconf.php
@@ -4,3 +4,4 @@ defined('TYPO3') or die('Access denied.');
 
 // Add default RTE configuration
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['kfv_template'] = 'EXT:kfv_template/Configuration/RTE/Default.yaml';
+
