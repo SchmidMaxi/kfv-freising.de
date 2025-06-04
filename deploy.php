ae99e8d94d5703927d32fcc8e5d893b7b716adeb
Maximilian Schmid <maxischmid92@gmx.de>
2025-06-04 14:34:35 +0200

    Deployment #1

--- deploy.php
+++ deploy.php
@@ -1,19 +1,17 @@
 <?php
 namespace Deployer;
 
-require 'recipe/common.php';
+require 'recipe/typo3.php';
 
 /**
  * Configuration
  */
 set('writable_mode', 'chmod');
-
-set('repository', 'git@github.com:SchmidMaxi/kfv-freising.de.git');
-
+set('repository', 'git@github.com-typo3:SchmidMaxi/kfv-freising.de.git');
 set('keep_releases', 3);
 set('typo3_webroot', 'public');
 set('ssh_multiplexing', false);
-
+set('bin/php', '/usr/bin/php83');
 
 // server configuration - STAGING
 host('staging')
@@ -31,6 +29,15 @@ host('production')
     ->set('deploy_path', '~/public_html/kfv-freising.de/v13/production')
     ->set('labels', ['stage' => 'production']);
 
+# Writeable directories
+add('writable_dirs', [
+    'config',
+    'var',
+    '{{typo3_webroot}}/_assets',
+    '{{typo3_webroot}}/fileadmin',
+    '{{typo3_webroot}}/typo3temp',
+]);
+
 # Shared directories
 add('shared_dirs', [
     'config',
@@ -44,28 +51,6 @@ add('shared_files', [
     'env.php',
     '{{typo3_webroot}}/.htaccess',
     '{{typo3_webroot}}/.htpasswd'
-]);
-
-# Writeable directories
-add('writable_dirs', [
-    'config',
-    'var',
-    '{{typo3_webroot}}/_assets',
-    '{{typo3_webroot}}/fileadmin',
-    '{{typo3_webroot}}/typo3temp',
-]);
-
-task('deploy:setup_typo3', function () {
-    cd('{{release_path}}');
-    run('/usr/bin/php83 composer.phar install --no-dev');
-    run('/usr/bin/php83 composer.phar dump-autoload');
-});
-
-desc('Deploys your project');
-task('deploy', [
-    'deploy:prepare',
-    'deploy:setup_typo3',
-    'deploy:publish',
 ]);
 
 after('deploy:failed', 'deploy:unlock');
