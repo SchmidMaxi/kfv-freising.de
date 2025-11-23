0630e74c817144c11d63f2860f9a6c92cbd4cd8b
Maximilian Schmid <maxischmid92@gmx.de>
2025-11-23 17:31:43 +0100

    update

--- packages/feuerwehren/Configuration/TCA/Overrides/tt_content.php
+++ packages/feuerwehren/Configuration/TCA/Overrides/tt_content.php
@@ -12,6 +12,15 @@ ExtensionUtility::registerPlugin(
     'Karte',
     'EXT:feuerwehren/Resources/Public/Icons/extension.svg'
 );
+// Fügt die FlexForm-Konfiguration zum Plugin hinzu
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['feuerwehren_karte'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'feuerwehren_karte',
+    'FILE:EXT:feuerwehren/Configuration/FlexForms/karte.xml'
+);
+
+
+
 ExtensionUtility::registerPlugin(
     'feuerwehren',
     'Organigramm',
@@ -24,10 +33,9 @@ ExtensionUtility::registerPlugin(
     'Jubiläen',
     'EXT:feuerwehren/Resources/Public/Icons/extension.svg'
 );
-
 // Fügt die FlexForm-Konfiguration zum Plugin hinzu
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['feuerwehren_jubilaeum'] = 'pi_flexform';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['feuerwehren_jubilaeen'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    'feuerwehren_jubilaeum',
+    'feuerwehren_jubilaeen',
     'FILE:EXT:feuerwehren/Configuration/FlexForms/jubilaeum.xml'
 );
